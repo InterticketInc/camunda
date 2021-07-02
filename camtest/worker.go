56c8bf665d2b43826fd732f36b546928cbbec51e
Gyula Paal <paalgyula@paalgyula.com>
2021-07-02 20:02:17 +0200

    Added variable simulation to worker

--- camtest/worker.go
+++ camtest/worker.go
@@ -3,15 +3,20 @@ package camtest
 import (
     "go.pirat.app/pi/camunda"
     "go.pirat.app/pi/camunda/worker"
+    "log"
+    "reflect"
+    "encoding/json"
 )
 
 type ContextStub struct {
     *worker.ContextImpl
+    failure bool
+    error   bool
+
+    vars camunda.Variables
 }
 
-func CreateTestContext() *ContextStub {
-    vars := camunda.Variables{}
-
+func CreateTestContext(vars camunda.Variables) *ContextStub {
     task := &camunda.ResLockedExternalTask{
         Variables:   vars,
         BusinessKey: "test-business-key",
@@ -19,29 +24,56 @@ func CreateTestContext() *ContextStub {
 
     return &ContextStub{
         ContextImpl: worker.NewContext(nil, task, "test-context-worker"),
+        vars:        vars,
+        failure:     false,
+        error:       false,
     }
 }
 
-func (w ContextStub) Complete(tc *worker.TaskComplete) error {
-    panic("implement me")
+func (w *ContextStub) Complete(tc *worker.TaskComplete) error {
+    if tc.Variables == nil {
+        return nil
+    }
+
+    for key, val := range tc.Variables {
+        if val.Type == "Object" && reflect.TypeOf(val.Value).Kind() == reflect.String {
+            m := make(map[string]interface{})
+            _ = json.Unmarshal([]byte(val.Value.(string)), &m)
+            val.Value = m
+        }
+
+        w.vars[key] = val
+    }
+
+    return nil
 }
 
-func (w ContextStub) HandleFailure(query worker.TaskFailureRequest) error {
-    panic("implement me")
+func (w *ContextStub) HandleFailure(query worker.TaskFailureRequest) error {
+    log.Printf("Failure handler called: %s Details: %s \n", query.ErrorMessage, query.ErrorDetails)
+    w.failure = true
+    return nil
 }
 
 func (w ContextStub) HandleBPMNError(code int, message string) error {
-    panic("implement me")
+    w.error = true
+
+    log.Printf("BPMN error received: %d message: %s\n", code, message)
+
+    return nil
 }
 
-func (w ContextStub) ExtendLock(id string, duration int) error {
-    panic("implement me")
+func (w ContextStub) ExtendLock(_ string, _ int) error {
+    return nil
 }
 
 func (w ContextStub) StartLockExtender() {
-    panic("implement me")
+    return
 }
 
 func (w ContextStub) StopExtender() {
-    panic("implement me")
+    return
+}
+
+func (w *ContextStub) Variables() camunda.Variables {
+    return w.vars
 }
--- variables.go
+++ variables.go
@@ -1,149 +1,155 @@
 package camunda
 
 import (
-	"encoding/json"
-	"fmt"
-	"strings"
+    "encoding/json"
+    "fmt"
+    "strings"
 
-	"github.com/mitchellh/mapstructure"
+    "github.com/mitchellh/mapstructure"
 )
 
 type Variables map[string]*Variable
 
 func (v Variables) String(name string) (string, error) {
-	if _, ok := v[name]; !ok {
-		return "", fmt.Errorf("variable '%s' not found", name)
-	}
+    if _, ok := v[name]; !ok {
+        return "", fmt.Errorf("variable '%s' not found", name)
+    }
 
-	if strings.ToLower(v[name].Type) != "string" {
-		return "", fmt.Errorf("cannot convert value type %s to string", v[name].Type)
-	}
+    if strings.ToLower(v[name].Type) != "string" {
+        return "", fmt.Errorf("cannot convert value type %s to string", v[name].Type)
+    }
 
-	return v[name].Value.(string), nil
+    return v[name].Value.(string), nil
 }
 
 func (v Variables) Int(name string) (int, error) {
-	if _, ok := v[name]; !ok {
-		return -1, fmt.Errorf("variable '%s' not found", name)
-	}
+    if _, ok := v[name]; !ok {
+        return -1, fmt.Errorf("variable '%s' not found", name)
+    }
 
-	t := strings.ToLower(v[name].Type)
-	if t != "Integer" && t != "Long" {
-		return -1, fmt.Errorf("cannot convert value type %s to Integer", v[name].Type)
-	}
+    t := strings.ToLower(v[name].Type)
+    if t != "Integer" && t != "Long" {
+        return -1, fmt.Errorf("cannot convert value type %s to Integer", v[name].Type)
+    }
 
-	return v[name].Value.(int), nil
+    return v[name].Value.(int), nil
 }
 
 func (v Variables) JSON(name string) ([]byte, error) {
-	if _, ok := v[name]; !ok {
-		return nil, fmt.Errorf("variable '%s' not found", name)
-	}
+    if _, ok := v[name]; !ok {
+        return nil, fmt.Errorf("variable '%s' not found", name)
+    }
 
-	if !strings.EqualFold(v[name].Type, "Json") {
-		return nil, fmt.Errorf("cannot convert value type %s to Json", v[name].Type)
-	}
+    if !strings.EqualFold(v[name].Type, "Json") {
+        return nil, fmt.Errorf("cannot convert value type %s to Json", v[name].Type)
+    }
 
-	return []byte(v[name].Value.(string)), nil
+    return []byte(v[name].Value.(string)), nil
 }
 
 // MarshalField marshals a field to the destination interface
+// nolint:wrapcheck
 func (v Variables) MarshalField(name string, field interface{}) error {
-	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
-		TagName: "json",
-		Result:  field,
-	})
+    decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+        TagName: "json",
+        Result:  field,
+    })
 
-	if err != nil {
-		return fmt.Errorf("cannot create decoder: %w", err)
-	}
+    if err != nil {
+        return fmt.Errorf("cannot create decoder: %w", err)
+    }
 
-	val, ok := v[name]
-	if !ok {
-		return fmt.Errorf("context variable not found: %s", name)
-	}
+    val, ok := v[name]
+    if !ok {
+        return fmt.Errorf("context variable not found: %s", name)
+    }
 
-	return decoder.Decode(val.Value)
+    switch val.Value.(type) {
+    case string:
+        return json.Unmarshal([]byte(val.Value.(string)), field)
+    default:
+        return decoder.Decode(val.Value)
+    }
 }
 
 // Map mapping values to original map format without type definitions and valueInfo fields
 func (v Variables) Map() map[string]interface{} {
-	m := make(map[string]interface{}, 0)
-	for k, val := range v {
-		m[k] = val.Value
-	}
+    m := make(map[string]interface{}, 0)
+    for k, val := range v {
+        m[k] = val.Value
+    }
 
-	return m
+    return m
 }
 
 func (v Variables) AddString(key string, value string) {
-	v[key] = &Variable{
-		Value: value,
-		Type:  "String",
-	}
+    v[key] = &Variable{
+        Value: value,
+        Type:  "String",
+    }
 }
 
 func (v Variables) AddJSON(key string, val interface{}) {
-	bb, _ := json.Marshal(val)
-	v.AddJSONBytes(key, bb)
+    bb, _ := json.Marshal(val)
+    v.AddJSONBytes(key, bb)
 }
 
 func (v Variables) AddJSONBytes(key string, bb []byte) {
-	v[key] = &Variable{
-		Value: string(bb),
-		Type:  "Json",
-	}
+    v[key] = &Variable{
+        Value: string(bb),
+        Type:  "Json",
+    }
 }
 
 func (v Variables) AddList(key string, values interface{}) {
-	jsonString, _ := json.Marshal(values)
+    jsonString, _ := json.Marshal(values)
 
-	v[key] = &Variable{
-		Value: string(jsonString),
-		Type:  "Object",
-		ValueInfo: &ValueInfo{
-			ObjectTypeName:          "java.util.ArrayList",
-			SerializationDataFormat: "application/json",
-		},
-	}
+    v[key] = &Variable{
+        Value: string(jsonString),
+        Type:  "Object",
+        ValueInfo: &ValueInfo{
+            ObjectTypeName:          "java.util.ArrayList",
+            SerializationDataFormat: "application/json",
+        },
+    }
 }
 
 func (v Variables) AddObject(key string, value interface{}) {
-	jsonString, _ := json.Marshal(value)
+    jsonString, _ := json.Marshal(value)
 
-	v[key] = &Variable{
-		Value: string(jsonString),
-		Type:  "Object",
-		ValueInfo: &ValueInfo{
-			ObjectTypeName:          "java.util.LinkedHashMap",
-			SerializationDataFormat: "application/json",
-		},
-	}
+    v[key] = &Variable{
+        Value: string(jsonString),
+        Type:  "Object",
+        ValueInfo: &ValueInfo{
+            ObjectTypeName:          "java.util.LinkedHashMap",
+            SerializationDataFormat: "application/json",
+        },
+    }
 }
 
 func CreateVariables(v map[string]interface{}) Variables {
-	vars := Variables{}
-	for k, val := range v {
-		tmp := Variable{
-			Value: val,
-			Type:  "Json",
-		}
+    vars := Variables{}
+    for k, val := range v {
+        tmp := Variable{
+            Value: val,
+            Type:  "Json",
+        }
 
-		switch val.(type) {
-		case float64, float32, int, int32, int64, int8, int16:
-			tmp.Type = "Long"
-		case string:
-			tmp.Type = "String"
-		case []byte: // already marshalled object
-			tmp.Value = string(val.([]byte))
-		default:
-			// Marshal object to JSON
-			bb, _ := json.Marshal(val)
-			tmp.Value = string(bb)
-		}
+        switch val.(type) {
+        case float64, float32, int, int32, int64, int8, int16:
+            tmp.Type = "Long"
+        case string:
+            tmp.Type = "String"
+        case []byte: // already marshalled object
+            tmp.Value = string(val.([]byte))
+        default:
+            // Marshal object to JSON
+            bb, _ := json.Marshal(val)
+            tmp.Value = string(bb)
+        }
 
-		vars[k] = &tmp
-	}
+        vars[k] = &tmp
+    }
 
-	return vars
+    return vars
 }
